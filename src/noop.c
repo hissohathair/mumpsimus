5f5de71b77c5e269ee5b5bd5d402827ffae7bd6b
hissohathair <dan@Daniels-MacBook-Pro.local>
2013-02-26 14:11:34 +1100

    Adding some debug output to noop to help test pipeif

--- src/noop.c
+++ src/noop.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 
 #include "util.h"
+#include "ulog.h"
 
 int main(int argc, char *argv[])
 {
@@ -17,25 +18,43 @@ int main(int argc, char *argv[])
   int     do_writes = 1;
   ssize_t rc = EX_OK;
 
+  size_t total_read = 0;
+  size_t total_wrote = 0;
+
   buf = (char*)malloc(sizeof(char) * SSIZE_MAX);
   if ( buf == NULL ) {
     perror("Error from malloc");
     return EX_OSERR;
   }
 
+  ulog_init(argv[0]);
+
   if ( strcmp(argv[0] + strlen(argv[0]) - 4, "null") == 0 )
     do_writes = 0;
+  ulog(LOG_INFO, "%s(%d): Starting %s (do_writes=%d)", __FILE__, __LINE__, argv[0], do_writes);
 
   do {
     memset(buf, 0, SSIZE_MAX);
     br = read(STDIN_FILENO, buf, SSIZE_MAX);
-    if ( do_writes && br > 1 )
-      write_all(STDOUT_FILENO, buf, br);
+    ulog_debug("%s: Read %zd bytes from fd=%d", argv[0], br, STDIN_FILENO);
+
+    if ( br > 0 )
+      total_read += (size_t)br;
     else if ( br < 0 )
       rc = EX_IOERR;
+
+    if ( do_writes && br >= 1 ) {
+      ssize_t nw = write_all(STDOUT_FILENO, buf, br);
+      if ( nw > 0 )
+	total_wrote += (size_t)nw;
+    }
+
   } while ( br > 0 );
 
+  ulog(LOG_INFO, "%s: EOF reached. Read %zd; Wrote %zd bytes. Exiting.", argv[0], total_read, total_wrote);
+
   free(buf);
+  ulog_close();
 
   return rc;
 }
